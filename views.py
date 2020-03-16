03eb7f4c649dec3e094896e52c8d35271772f099
HaRiNaTh93 <reddy13693@gmail.com>
2020-03-16 17:50:18 +0530

    changes

---
+++ views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,'index.html')
+
+# def concat(request):
+#     fn=request.POST['fname']
+#     ln=request.POST['lname']
+#     ff=fn+ln
+#     return render(request,"display.html",{'display':ff})
